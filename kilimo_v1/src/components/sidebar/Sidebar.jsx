0bddf6ee80f00ad132c8e43be2263af005984c58
edson <firstedson@gmail.com>
2023-10-15 12:19:11 +0300

    configuring dashboard

--- kilimo_v1/src/components/sidebar/Sidebar.jsx
+++ kilimo_v1/src/components/sidebar/Sidebar.jsx
@@ -5,6 +5,13 @@ import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 import CategoryRoundedIcon from '@mui/icons-material/CategoryRounded';
 import GradingRoundedIcon from '@mui/icons-material/GradingRounded';
 import LocalShippingRoundedIcon from '@mui/icons-material/LocalShippingRounded';
+import AssessmentRoundedIcon from '@mui/icons-material/AssessmentRounded';
+import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
+import MonitorHeartRoundedIcon from '@mui/icons-material/MonitorHeartRounded';
+import ViewStreamRoundedIcon from '@mui/icons-material/ViewStreamRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import AccountBoxRoundedIcon from '@mui/icons-material/AccountBoxRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 
 
 
@@ -17,10 +24,13 @@ const Sidebar = () => {
             <hr/>
             <div className="center">
                 <ul>
+                    <p className="title">MAIN</p>
                     <li>
                         <SpaceDashboardRoundedIcon/>
                         <span>Dashboard</span>
                     </li>
+
+                    <p className="title">LISTS</p>
                     <li>
                         <PersonRoundedIcon/>
                         <span>Users</span>
@@ -37,30 +47,46 @@ const Sidebar = () => {
                         <LocalShippingRoundedIcon/>
                         <span>Delivery</span>
                     </li>
+
+                    <p className="title">USEFUL</p>
                     <li>
+                        <AssessmentRoundedIcon/>
                         <span>Stats</span>
                     </li>
                     <li>
+                        <NotificationsRoundedIcon/>
                         <span>Notifications</span>
                     </li>
+
+                    <p className="title">SERVICES</p>
                     <li>
+                        <MonitorHeartRoundedIcon/>
                         <span>System Health</span>
                     </li>
                     <li>
+                        <ViewStreamRoundedIcon/>
                         <span>Logs</span>
                     </li>
                     <li>
+                        <SettingsRoundedIcon/>
                         <span>Settings</span>
                     </li>
+
+                    <p className="title">USER</p>
                     <li>
+                        <AccountBoxRoundedIcon/>
                         <span>Profile</span>
                     </li>
                     <li>
+                        <LogoutRoundedIcon/>
                         <span>Log out</span>
                     </li>
                 </ul>
             </div>
-            <div className="bottom">color options</div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
         </div>
     )
 }

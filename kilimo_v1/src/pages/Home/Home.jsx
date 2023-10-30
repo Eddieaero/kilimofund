4ba38108a74273cf9e9f5dcb2552e6a52f87a5f8
edson <firstedson@gmail.com>
2023-10-30 23:14:50 +0300

    configuring pages

---
+++ kilimo_v1/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+import "home.scss";
+import Sidebar from "../../components/SideBar Section/Sidebar";
+import Body from "../../components/Body section/Body";
+
+
+
+const Home = () => {
+
+    <div className="HomeSection">
+        <Sidebar/>
+        <Body/>
+
+        home
+    </div>
+
+
+}
+
+
+export default Home

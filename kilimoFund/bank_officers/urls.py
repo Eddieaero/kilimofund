2ad0e6adbb0568def4d7086dbb4fe052d000cc31
eddygulled <edgargulay69@gmail.com>
2023-10-30 13:21:44 +0300

    init database for officers

--- kilimoFund/bank_officers/urls.py
+++ kilimoFund/bank_officers/urls.py
@@ -4,6 +4,8 @@ from .views import *
 app_name = 'kilimoFund'
 
 urlpatterns = [
+    path('signup/', bank_officer_signup, name='bank_officer_signup'),
+    path('login/', bank_officer_login, name='bank_officer_login'),
     path('send-message/', send_message, name='send-message'),
     path('get-messages/<int:receiver_id>/', get_messages, name='get-messages'),
     path('loan-application-guidelines/', get_loan_application_guidelines, name='loan-application-guidelines'),
